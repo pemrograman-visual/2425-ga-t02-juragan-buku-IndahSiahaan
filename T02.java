eff8374fa47a19e010f30e030cedd24ad1279225
IndahSiahaan <triyunisiahaan@gmail.com>
2024-09-27 10:42:11 +0700

    Sudah menyelesaikan tugas

--- T02.java
+++ T02.java
@@ -1,1 +1,47 @@
-// NIM - Your Name
+// 12S24052 - Indah Triyuni Siahaan
+// 12S24007 - Dasmauli Sormin
+
+import java.util.*;
+import java.lang.Math;
+
+public class T02 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String iSBN, judul, penulis, penerbit, formatBukuElektronik, kategori;
+        int tahunTerbit, stok, rating;
+        double hargaPembelian, minimumMargin;
+
+        iSBN = input.nextLine();
+        judul = input.nextLine();
+        penulis = input.nextLine();
+        penerbit = input.nextLine();
+        formatBukuElektronik = input.nextLine();
+        tahunTerbit = input.nextInt();
+        stok = input.nextInt();
+        hargaPembelian = input.nextDouble();
+        minimumMargin = input.nextDouble();
+        rating = input.nextInt();
+        if (rating >= 4.7) {
+            kategori = "Best Pick";
+        } else {
+            if (rating >= 4.5) {
+                kategori = "Must Read";
+            } else {
+                if (rating >= 4.0) {
+                    kategori = "Recommended";
+                } else {
+                    if (rating >= 3.0) {
+                        kategori = "Average";
+                    } else {
+                        if (rating <= 3.0) {
+                            kategori = "Low";
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(iSBN + "|" + judul + "|" + penulis + "|" + tahunTerbit + "|" + penerbit + "|" + formatBukuElektronik + "|" + hargaPembelian + "|" + minimumMargin + "|" + stok + "|" + rating + "|" + kategori);
+    }
+}
+
